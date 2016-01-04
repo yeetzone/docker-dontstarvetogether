6ce5331acd70f366001e63b62e9f0314b0740c1f
Thomas Deinhamer <thasmo@gmail.com>
2016-01-04 19:51:35 +0100

    Update Vagrantfile

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update Packages
+apt-get update
+
+# Install Dependencies
+apt-get install -y curl dnsmasq linux-image-extra-$(uname -r)
+sleep 3
+
+# Install Docker Engine
+curl -sSL https://get.docker.com/ | sh
+
+# Install Docker Compose
+curl -sSL https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Add the vagrant-user to the Docker group
+usermod -aG docker vagrant
