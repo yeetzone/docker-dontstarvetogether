24578a0a5b792522310641199f4cb40bccde17e1
Brais Gabin <braisgabin@gmail.com>
2016-01-11 12:43:10 +0100

    Test that the entrypoint exits with the correct status code

--- test/entrypoint-other/run.sh
+++ test/entrypoint-other/run.sh
@@ -10,3 +10,6 @@ file2=`mktemp`
 docker run --rm $1 echo OK > $file1 || exit 1
 echo OK > $file2
 cmp --silent $file1 $file2 || exit 1
+
+../build/docker-entrypoint.sh false && exit 1
+../build/docker-entrypoint.sh true || exit 1
