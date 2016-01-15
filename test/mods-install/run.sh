750f447cde19a0c6d4c1cfbf74884069bdb6a9e5
Brais Gabin <braisgabin@gmail.com>
2016-01-15 14:14:28 +0100

    Remove obsolete UPDATE_ON_BOOT variable

--- test/mods-install/run.sh
+++ test/mods-install/run.sh
@@ -10,7 +10,7 @@ clean() {
 trap clean EXIT
 
 log_txt=`mktemp`
-container_id=`docker run -e MODS=492173795 -e UPDATE_ON_BOOT=false -d $1 || exit 1`
+container_id=`docker run -e MODS=492173795 -d $1 || exit 1`
 sleep 20 # We need to wait for the server to start
 docker cp $container_id:/var/lib/dsta/config/log.txt $log_txt || exit 1
 if grep -Fq "Failed to create mod folder [../mods/" $log_txt
