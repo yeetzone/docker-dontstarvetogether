b694ffec0046e8d74df8ae3826f5a3802e58a3b5
Brais Gabin <braisgabin@gmail.com>
2016-01-20 20:23:03 +0100

    Add a way to send commands to the console

---
+++ test/console/commands.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo foo > /usr/local/lib/dsta/console
+echo bar > /usr/local/lib/dsta/console
---
+++ test/console/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+clean() {
+	rm -f $file1
+	if [ -n "`docker ps -qf id=$container_id`" ]; then
+		docker rm -fv $container_id > /dev/null
+	fi
+}
+trap clean EXIT
+
+file1=`mktemp`
+
+container_id=`docker run -d $1 || exit 1`
+docker cp console/commands.sh $container_id:/
+docker exec $container_id /commands.sh || exit 1
+sleep 20
+docker cp $container_id:/var/lib/dsta/config/log.txt $file1
+grep -Fq "ConsoleInput: \"foo\"" $file1 || exit 1
+grep -Fq "ConsoleInput: \"bar\"" $file1 || exit 1
+docker rm -fv $container_id > /dev/null
