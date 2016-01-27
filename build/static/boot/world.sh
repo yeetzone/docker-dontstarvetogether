64513798b0690650a6092bdb69fe7cab5dcf17e4
Brais Gabin <braisgabin@gmail.com>
2016-01-27 21:01:14 +0100

    Validate environment variables. Close #28

--- build/static/boot/world.sh
+++ build/static/boot/world.sh
@@ -9,6 +9,10 @@ fi
 if [ -n "$WORLD_OVERRIDES" ]; then
 	echo "$WORLD_OVERRIDES" > $file_world_override
 elif [ -n "$WORLD_PRESET" ]; then
+	source "`dirname "$0"`/aux.sh"
+
+	validate_option "WORLD_PRESET" SURVIVAL_TOGETHER SURVIVAL_TOGETHER_CLASSIC SURVIVAL_DEFAULT_PLUS COMPLETE_DARKNESS DST_CAVE
+
 	cat <<- EOF > $file_world_override
 return {
   override_enabled = true,
