7fdf23a5df9e9bd50cc265055811246a9626db14
Thomas Deinhamer <thasmo@gmail.com>
2016-08-11 00:40:10 +0200

    Optimize the entrypoint script

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-if [ $1 == "dst-server" ]; then
-	set -e
+# Exit immediately on non-zero return codes.
+set -e
 
-	# Configure the server
+# Run boot scripts before starting the server.
+if [ $1 = 'dst-server' ]; then
+
+	# Prepare the shard directory.
 	mkdir -p $CLUSTER_PATH/$SHARD_NAME
 	chown -R $STEAM_USER:$STEAM_USER $CLUSTER_PATH
 
-	if [ "$2" == "start" ]; then
+	# Create configuration files.
+	if [ "$2" = 'start' ]; then
 		$DSTA_HOME/boot/token.sh
 		$DSTA_HOME/boot/settings.sh
 		$DSTA_HOME/boot/lists.sh
 		$DSTA_HOME/boot/world.sh
 		$DSTA_HOME/boot/mods.sh
 	fi
+
+	# Run as user `steam` if the command is `dst-server`.
+	set -- gosu steam "$@"
 fi
 
+# Execute the command.
 exec "$@"
