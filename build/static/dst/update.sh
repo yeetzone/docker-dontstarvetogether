e478a7e399178fcf24648abc9ce4800a5d974015
Thomas Deinhamer <thasmo@gmail.com>
2016-08-13 11:22:20 +0200

    Support installation of beta branches (#74)

--- build/static/dst/update.sh
+++ build/static/dst/update.sh
@@ -5,4 +5,6 @@ exec steamcmd \
 	+login anonymous \
 	+force_install_dir $DST_HOME \
 	+app_update 343050 \
+		$([ -n "$DST_BRANCH" ] && printf %s "-beta $DST_BRANCH") \
+		$([ -n "$DST_BRANCH_PASSWORD" ] && printf %s "-betapassword $DST_BRANCH_PASSWORD") \
 	+quit
