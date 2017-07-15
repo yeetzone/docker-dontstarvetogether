2bfc9ac4044adda6b2721e94d7f85b2d78af6fc4
Brais Gabín <braisgabin@gmail.com>
2017-07-15 15:15:40 +0200

    Fix a strange BC in debian:9 (#91)

--- test/close-cleanly/run.sh
+++ test/close-cleanly/run.sh
@@ -30,7 +30,7 @@ grep -q "Shutting down$" $file1 || exit 1
 # TERM
 container_id=`docker run -d $1 dst-server start --update=none || exit 1`
 sleep 20
-docker exec $container_id kill 1
+docker exec $container_id bash -c "kill 1"
 timeout --kill-after 1 12 docker wait $container_id > $file1
 grep -q "^0$" $file1 || exit 1
 docker cp $container_id:/var/lib/dsta/cluster/shard/server_log.txt $file1
@@ -40,7 +40,7 @@ grep -q "Shutting down$" $file1 || exit 1
 # INT
 container_id=`docker run -d $1 dst-server start --update=none || exit 1`
 sleep 20
-docker exec $container_id kill -SIGINT 1
+docker exec $container_id bash -c "kill -SIGINT 1"
 timeout --kill-after 1 12 docker wait $container_id > $file1
 grep -q "^0$" $file1 || exit 1
 docker cp $container_id:/var/lib/dsta/cluster/shard/server_log.txt $file1
