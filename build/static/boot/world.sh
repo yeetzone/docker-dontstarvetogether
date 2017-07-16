934e87b18049022b48fb94e9a203edac08f1a3d7
Brais Gabín <braisgabin@gmail.com>
2017-07-16 18:35:44 +0200

    Implement keep-configuration world

--- build/static/boot/world.sh
+++ build/static/boot/world.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+source "`dirname "$0"`/functions.sh"
+
 file_leveldata_override="$CLUSTER_PATH/$SHARD_NAME/leveldataoverride.lua"
 
-if [ -f $file_leveldata_override ]; then
+if [[ -f "$file_leveldata_override" ]] && containsElement "world" $@ ; then
 	exit 0
 fi
 
-if [ -n "$LEVELDATA_OVERRIDES" ]; then
-	echo "$LEVELDATA_OVERRIDES" > $file_leveldata_override
-elif [ -n "$WORLD_PRESET" ]; then
-	source "`dirname "$0"`/functions.sh"
+rm -f $file_leveldata_override
 
+if [[ -n "$LEVELDATA_OVERRIDES" ]]; then
+	echo "$LEVELDATA_OVERRIDES" > $file_leveldata_override
+elif [[ -n "$WORLD_PRESET" ]]; then
 	validate_option "WORLD_PRESET" SURVIVAL_TOGETHER SURVIVAL_TOGETHER_CLASSIC SURVIVAL_DEFAULT_PLUS COMPLETE_DARKNESS DST_CAVE
 
 	cat <<- EOF > $file_leveldata_override
@@ -21,6 +23,6 @@ return {
 EOF
 fi
 
-if [ -f $file_leveldata_override ]; then
+if [[ -f $file_leveldata_override ]]; then
 	chown $STEAM_USER:$STEAM_USER $file_world_override
 fi
