d6fa3be9f39840964ec5a72d42fffb22ddef1fa2
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-10-22 19:07:09 +0200

    Remove defect mod from examples (#143)
    The mod #492173795 (aka Moderator Commands) is throwing the error
    mentioned in issue #142. Since this is only an example, I'm removing the
    mod.
    
    Ticket: https://github.com/fairplay-zone/docker-dontstarvetogether/issues/142
    Mod: https://steamcommunity.com/sharedfiles/filedetails/?id=492173795
    
    Signed-off-by: Marcos Loureiro <mloureiro.dev@gmail.com>

--- docs/examples/mods/modoverrides.lua
+++ docs/examples/mods/modoverrides.lua
@@ -1,6 +1,5 @@
 return {
 	['workshop-378160973'] = { enabled = true },
-	['workshop-492173795'] = { enabled = true },
 	['workshop-407705132'] = { enabled = true,
 		configuration_options = {
 			title = 'Welcome!',
