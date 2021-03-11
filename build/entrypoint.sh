2fbfc2fe00b4362e124e467507546a797589f86a
Thomas Deinhamer <thasmo@thasmo.com>
2021-03-11 17:14:00 +0100

    Fix permission issues

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,6 +7,9 @@ if [[ "${1:0:1}" = '-' ]]; then
 fi
 
 if [[ "$1" = 'dontstarvetogether' ]]; then
+	mkdir -p "$STORAGE_PATH/$CLUSTER_NAME/$SHARD_NAME"
+	chown -R "$STEAM_USER":"$STEAM_USER" "$STORAGE_PATH"
+
 	gosu "$STEAM_USER" "$STEAM_HOME/scripts/boot/token.sh"
 	gosu "$STEAM_USER" "$STEAM_HOME/scripts/boot/settings.sh"
 	gosu "$STEAM_USER" "$STEAM_HOME/scripts/boot/lists.sh"
--- build/scripts/boot/token.sh
+++ build/scripts/boot/token.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-file="$STORAGE_PATH"/$CLUSTER_NAME/cluster_token.txt
+file="$STORAGE_PATH/$CLUSTER_NAME/cluster_token.txt"
 
 if [[ -n "$TOKEN" ]] && [[ ! -f "$file" ]]; then
 	echo "$TOKEN" > "$file"
