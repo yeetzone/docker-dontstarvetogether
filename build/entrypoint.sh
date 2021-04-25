40cf5b20b612da6122327a94f9b9d410de10e6d4
Thomas Deinhamer <thasmo@thasmo.com>
2021-04-25 13:08:24 +0200

    Change owner on game data directory recursively on boot

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -9,6 +9,7 @@ fi
 if [[ "$1" = 'dontstarvetogether' ]]; then
 	mkdir -p "$STORAGE_PATH/$CLUSTER_NAME/$SHARD_NAME"
 	chown -R "$STEAM_USER":"$STEAM_USER" "$STORAGE_PATH"
+	chown -R "$STEAM_USER":"$STEAM_USER" "$STEAM_PATH"
 
 	gosu "$STEAM_USER" "$STEAM_HOME/scripts/boot/token.sh"
 	gosu "$STEAM_USER" "$STEAM_HOME/scripts/boot/settings.sh"
